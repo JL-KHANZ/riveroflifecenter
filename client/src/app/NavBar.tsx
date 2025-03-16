3ab30c3a083465fa4e28324670a14302cf92fede
josh lim <joshlim@joshs-MacBook-Air.local>
2025-03-16 22:26:01 +0900

    nothing yet

---
+++ client/src/app/NavBar.tsx
@@ -0,0 +1,14 @@
+
+export default function NavBar() {
+    return <nav className="nav">
+        <a href="/">River Of Life Center</a>
+        <ul>
+            <li>
+                <a href="/userPage">User</a>
+            </li>
+            <li>
+                logout
+            </li>
+        </ul>
+    </nav>
+}
